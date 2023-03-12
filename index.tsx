080e3e5a59869225f4c52677c5bafdc321c0cdab
Akash Shukla <shuklaa884@gmail.com>
2023-03-12 18:37:38 +0530

    deploy

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './src/App'
 import { store } from './src/redux/store'
 
@@ -9,13 +10,11 @@ const container =
   document.getElementById('root') || document.createElement('div')
 
 createRoot(container).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <HashRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </HashRouter>
 )
 // root.render(<App/>);
 // check
